8809821ff58dd6561390706ad1f000c709304539
ivannserignoleba-afk <ivannserignoleba@gmail.com>
2026-08-20 11:44:44 +0000

    Corriger serveur Express 5

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const app=express();
 app.use(express.json());
 app.get('/api/health',(req,res)=>res.json({ok:true,service:'EDK Bénédiction'}));
 app.use(express.static(path.join(__dirname,'../dist')));
-app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'../dist/index.html')));
+app.use((req,res)=>res.sendFile(path.join(__dirname,'../dist/index.html')));
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>console.log(`EDK Bénédiction server running on port ${PORT}`));
